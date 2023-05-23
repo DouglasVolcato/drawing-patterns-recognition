6491f8851418bbca0cb85249466fb7cee89ad64b
Douglas Volcato <douglasvolcato@gmail.com>
2023-05-23 01:02:32 -0300

    chore: implement styles and group by key util method

--- common/utils.js
+++ common/utils.js
@@ -13,6 +13,30 @@ utils.printProgress = (count, max) => {
   process.stdout.write(count + "/" + max + " (" + percent + ")");
 };
 
+utils.groupBy = (objArray, key) => {
+  const groups = {};
+
+  for (let obj of objArray) {
+    const val = obj[key];
+    if (groups[val] == null) {
+      groups[val] = [];
+    }
+    groups[val].push(obj);
+  }
+  return groups;
+};
+
+utils.styles = {
+  car: { color: "gray", emoji: "🚗" },
+  fish: { color: "red", emoji: "🐠" },
+  house: { color: "yellow", emoji: "🏠" },
+  tree: { color: "green", emoji: "🌳" },
+  bicycle: { color: "cyan", emoji: "🚲" },
+  guitar: { color: "blue", emoji: "🎸" },
+  pencil: { color: "magenta", emoji: "✏️" },
+  clock: { color: "lightgray", emoji: "⏰" },
+};
+
 if (typeof module !== "undefined") {
   module.exports = utils;
 }
